1e860f34148383586ec3427004ad53802485e420
forfirstgundam <90223382+forfirstgundam@users.noreply.github.com>
2025-07-11 16:19:18 +0900

    Fix | Player to use characterbehaviour

--- Assets/02. Scripts/Util/CharacterFactory/CharacterBehaviour.cs
+++ Assets/02. Scripts/Util/CharacterFactory/CharacterBehaviour.cs
@@ -1,7 +1,9 @@
+using Photon.Pun;
 using UnityEngine;
+using UnityEngine.InputSystem;
 using UnityEngine.Rendering;
 
-public class CharacterBehaviour : MonoBehaviour
+public class CharacterBehaviour : PlayerActivity
 {
     [SerializeField] private ECharacterName _characterName;
 
@@ -10,8 +12,9 @@ public class CharacterBehaviour : MonoBehaviour
     private bool _isInitialized = false;
 
     // 캐릭터 스킬 조합
-    private async void Start()
+    protected override async void Start()
     {
+        base.Start();
         if (!CharacterManager.Instance.IsInitialized) 
         {
             await CharacterManager.Instance.Init();
@@ -30,7 +33,27 @@ public class CharacterBehaviour : MonoBehaviour
 
         _isInitialized = true;
     }
-    
+
+    public void OnAttack(InputAction.CallbackContext callback)
+    {
+        if (!_photonView.IsMine) return;
+
+        if (callback.performed)
+        {
+            // 일반 공격을 함
+            Debug.Log("Normal Attack performed");
+            _character.UseSkill(ESkillType.BasicAttack);
+            _photonView.RPC(nameof(RPC_NormalAttack), RpcTarget.All);
+        }
+    }
+
+    [PunRPC]
+    private void RPC_NormalAttack()
+    {
+        Debug.Log("Normal Attack performed via RPC");
+        _character.UseSkill(ESkillType.BasicAttack);
+    }
+
     private void Update()
     {
         if(_isInitialized == false)
@@ -39,10 +62,10 @@ public class CharacterBehaviour : MonoBehaviour
         }
         //_character.Update();
 
-        if (Input.GetMouseButtonDown(0))
-        {
-            _character.UseSkill(ESkillType.BasicAttack);
-        }
+        //if (Input.GetMouseButtonDown(0))
+        //{
+        //    _character.UseSkill(ESkillType.BasicAttack);
+        //}
         if (Input.GetKeyDown(KeyCode.Q))
         {
             _character.UseSkill(ESkillType.Skill);
