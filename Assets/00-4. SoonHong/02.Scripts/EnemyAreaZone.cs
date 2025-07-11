67965a24b6239a2190b65bde70d6e8f2923f581c
ParkSoonHong <95293014+ParkSoonHong@users.noreply.github.com>
2025-07-11 17:47:48 +0900

    [박순홍] 퇴근 커밋

--- Assets/00-4. SoonHong/02.Scripts/EnemyAreaZone.cs
+++ Assets/00-4. SoonHong/02.Scripts/EnemyAreaZone.cs
@@ -6,7 +6,7 @@ public class EnemyAreaZone : MonoBehaviour
     [Header("Area Settings")]
     public float AreaRadius = 10f;
     public Color GizmoColor = new Color(0, 1, 0, 0.25f); // 반투명 녹색
-    public List<GameObject> PatrolPoints;
+  //  public List<GameObject> PatrolPoints;
 
     private void OnDrawGizmos()
     {
