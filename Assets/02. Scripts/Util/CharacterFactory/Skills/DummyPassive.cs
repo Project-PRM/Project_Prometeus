1462a6456b3696ecb5fd4bff89c92e8c7f8c6a0d
Sumin Cha <109404269+goliot@users.noreply.github.com>
2025-07-14 13:06:11 +0900

    Feat | 군중 제어기, 패시브 효과 등 발동 기본 틀 구현

--- Assets/02. Scripts/Util/CharacterFactory/Skills/DummyPassive.cs
+++ Assets/02. Scripts/Util/CharacterFactory/Skills/DummyPassive.cs
@@ -1,7 +1,7 @@
 using UnityEngine;
 
 [Skill("DummyPassive")]
-public class DummyPassive : ISkill
+public class DummyPassive : IEventReactiveSkill
 {
     private float _timer = 0f;
 
@@ -15,5 +15,15 @@ public class DummyPassive : ISkill
     public void Activate(CharacterBase character)
     {
         Debug.Log($"{character.Name} activated DummyPassive.");
+    }
+
+    public void OnEvent(ECharacterEvent evt)
+    {
+        if(evt != ECharacterEvent.OnBasicAttack)
+        {
+            return;
+        }
+
+        Debug.Log($"패시브 추뎀 발동 : {Data.Damage}");
     }
 }
