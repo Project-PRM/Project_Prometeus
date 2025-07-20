1ee6433c45c4444575cfc23b504d5b7593535ae2
kumaotra <kimkyoungho0726@gmail.com>
2025-07-21 00:50:28 +0900

    [KKH] feat. 친구 기능 완료
    친구 목록확인, 친구 요청, 친구 요청 목록확인, 요청수락/거절 까지

--- Assets/00-2. KyeongHo/02.Scripts/Friend/4.UI/UI_RequestFriendslot.cs
+++ Assets/00-2. KyeongHo/02.Scripts/Friend/4.UI/UI_RequestFriendslot.cs
@@ -7,18 +7,22 @@ public class UI_RequestFriendslot : MonoBehaviour
     public TextMeshProUGUI SenderNickname;
     public Button AcceptButton;
     public Button DenyButton;
-
-    public void Refresh(string nickname)
+    private string _uid;
+    
+    public void Refresh(string nickname, string uid)
     {
         SenderNickname.text = nickname;
+        _uid = uid;
     }
 
-    public void OnClickAccept()
+    public async void OnClickAccept()
     {
-        
+        await FriendManager.Instance.AcceptFriendRequest(AccountManager.Instance.MyAccount.UserId, _uid);
+        Destroy(gameObject);
     }
-    public void OnClickDeny()
+    public async void OnClickDecline()
     {
-        
+        await FriendManager.Instance.DeclineFriendRequest(AccountManager.Instance.MyAccount.UserId, _uid);
+        Destroy(gameObject);
     }
 }
