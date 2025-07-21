68c2afbe6ed2977dbe4082b3bbfb0b7872509e00
forfirstgundam <90223382+forfirstgundam@users.noreply.github.com>
2025-07-21 17:15:57 +0900

    Fix | Trying to link Healthbar

--- Assets/00-5. YeoJin/02.Scripts/Character/UI/UI_HealthBar.cs
+++ Assets/00-5. YeoJin/02.Scripts/Character/UI/UI_HealthBar.cs
@@ -2,9 +2,18 @@ using UnityEngine;
 
 public class UI_HealthBar : MonoBehaviour
 {
-    void Start()
+    [SerializeField] private RectTransform Fill;
+
+    private void Start()
     {
-        
+        Fill.localScale = new Vector3(5, 0.2f, 1);
+    }
+
+    public void SetValue(float currenthealth)
+    {
+        Vector3 scale = Fill.localScale;
+        scale.x = currenthealth / 5f;
+        Fill.localScale = scale;
     }
 
     private void Update()
