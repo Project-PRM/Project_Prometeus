05d3733b2b1351d17568705b1c7c62df1e4e1ba3
forfirstgundam <90223382+forfirstgundam@users.noreply.github.com>
2025-07-22 18:52:04 +0900

    Feat | Health bar (characteringameview)

--- Assets/00-5. YeoJin/02.Scripts/Character/CharacterActivity/CharacterIngameView.cs
+++ Assets/00-5. YeoJin/02.Scripts/Character/CharacterActivity/CharacterIngameView.cs
@@ -6,12 +6,14 @@ using UnityEngine.TextCore.Text;
 using WebSocketSharp;
 
 [RequireComponent(typeof(PhotonView))]
-public class CharacterInGameView : MonoBehaviour
+public class CharacterInGameView : MonoBehaviour, IPunObservable
 {
     [SerializeField] private UI_NicknameIngame _nickname;
     [SerializeField] private UI_HealthBar _healthBar;
     private CharacterBehaviour _characterBehaviour;
     private CharacterBase _character;
+
+    private float _networkHealthRatio = 1f;
 
     private void Awake()
     {
@@ -37,11 +39,27 @@ public class CharacterInGameView : MonoBehaviour
 
     public void OnTakenDamage(ECharacterEvent characterEvent)
     {
-        if (_character == null) _character = _characterBehaviour.GetCharacterBase();
+        if (_character == null)
+            _character = _characterBehaviour.GetCharacterBase();
+
         if (characterEvent == ECharacterEvent.OnDamaged || characterEvent == ECharacterEvent.OnDeath)
         {
-            _healthBar.SetValue(_character.CurrentHealth / _character.BaseStats.MaxHealth);
+            float healthRatio = _character.CurrentHealth / _character.BaseStats.MaxHealth;
+
+            // 내 캐릭터인 경우에만 RPC 호출
+            if (_characterBehaviour.PhotonView.IsMine)
+            {
+                // RPC로 모든 클라이언트에 체력바 업데이트 전송
+                _characterBehaviour.PhotonView.RPC("UpdateHealthBarRPC", RpcTarget.All, healthRatio);
+            }
         }
+    }
+
+    [PunRPC]
+    void UpdateHealthBarRPC(float healthRatio)
+    {
+        _networkHealthRatio = healthRatio;
+        _healthBar.SetValue(healthRatio);
     }
 
     private void SetupCamera()
@@ -71,6 +89,34 @@ public class CharacterInGameView : MonoBehaviour
         if (myTeam == team)
         {
             revealer.enabled = true;
+        }
+    }
+
+    public void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info)
+    {
+        if (stream.IsWriting)
+        {
+            // 데이터 전송 (마스터가 보내는 경우)
+            if (_character != null)
+            {
+                float healthRatio = _character.CurrentHealth / _character.BaseStats.MaxHealth;
+                stream.SendNext(healthRatio);
+            }
+            else
+            {
+                stream.SendNext(_networkHealthRatio);
+            }
+        }
+        else
+        {
+            // 데이터 수신
+            _networkHealthRatio = (float)stream.ReceiveNext();
+
+            // 내 캐릭터가 아닌 경우에만 체력바 업데이트
+            if (!_characterBehaviour.PhotonView.IsMine)
+            {
+                _healthBar.SetValue(_networkHealthRatio);
+            }
         }
     }
 }
