f055be02cc0029650f1d915f9c408053dd3ce7ae
forfirstgundam <90223382+forfirstgundam@users.noreply.github.com>
2025-07-24 16:13:55 +0900

    Fix | RoomManager spawns Attacker

--- Assets/02. Scripts/Util/CharacterFactory/Skills/FulfunsPassive.cs
+++ Assets/02. Scripts/Util/CharacterFactory/Skills/FulfunsPassive.cs
@@ -1,7 +1,12 @@
 using UnityEngine;
 
-public class FulfunsPassive : IPermanentSkill
+public class FulfunsPassive : IEventReactiveSkill
 {
+    private CharacterBase _character;
+    private bool _isTracking = false;
+    private float _activeTime = 5f;
+    private float _cooldown = 1f;
+    private float _lastSpawnTime = -Mathf.Infinity;
     public SkillData Data { get; set; }
 
     public void Update()
@@ -16,5 +21,10 @@ public class FulfunsPassive : IPermanentSkill
     public void Activate(CharacterBase character)
     {
         Debug.Log("Activated FulfunsPassive");
+    }
+
+    public void OnEvent(ECharacterEvent evt)
+    {
+
     }
 }
